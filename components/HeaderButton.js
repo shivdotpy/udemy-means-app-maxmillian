66e6f13f92009d7b5c4b75015fee2b2bb013a50a
shiv <shivssharma1990@gmail.com>
2020-04-22 16:28:14 +0530

    favorite button --bug

---
+++ components/HeaderButton.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View, Platform } from 'react-native';
+import { HeaderButton } from 'react-navigation-header-buttons';
+import { Ionicons } from '@expo/vector-icons';
+import Colors from '../constants/Colors';
+
+const CustomHeaderButton = (props) => {
+	return (
+		<HeaderButton
+			{...props}
+			IconComponent={Ionicons}
+			iconSize={23}
+			color={Platform === 'android' ? 'white' : Colors.primaryColor}
+		/>
+	);
+};
+
+const styles = StyleSheet.create({});
+
+export default CustomHeaderButton;
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { MEALS } from '../data/dummy-data';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import CustomHeaderButton from '../components/HeaderButton';
 
 const MealDetailScreen = (props) => {
 	const mealId = props.navigation.getParam('mealId');
@@ -21,7 +23,18 @@ MealDetailScreen.navigationOptions = (navigationData) => {
 	const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
 	return {
-		headerTitle: selectedMeal.title
+		headerTitle: selectedMeal.title,
+		headerRight: () => (
+			<HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+				<Item
+					title="Favorite"
+					iconName="ios-star"
+					onPress={() => {
+						console.log('Mark as favorite');
+					}}
+				/>
+			</HeaderButtons>
+		)
 	};
 };
 
